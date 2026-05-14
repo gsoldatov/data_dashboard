bc88e9bf6a9450bffddd493d545169d3b6d474ee
- <smoke571reg@yandex.ru>
2026-05-14 18:11:36 +0000

    Renamed file

--- dashboard_backend/src/models/__init__.py
+++ dashboard_backend/src/models/__init__.py
@@ -1,4 +1,4 @@
 from dashboard_backend.src.models.user import UserCreate as UserCreate, UserUpdate as UserUpdate, UserResponse as UserResponse, User as User  # noqa: F401
 from dashboard_backend.src.models.session import LoginRequest as LoginRequest, SessionResponse as SessionResponse, Session as Session  # noqa: F401
-from dashboard_backend.src.models.page import PageSettingsUpsert as PageSettingsUpsert, PageSettings as PageSettings  # noqa: F401
+from dashboard_backend.src.models.page_settings import PageSettingsUpsert as PageSettingsUpsert, PageSettings as PageSettings  # noqa: F401
 from dashboard_backend.src.models.common import AtLeastOneFieldSetMixin as AtLeastOneFieldSetMixin  # noqa: F401
