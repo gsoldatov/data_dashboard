31f91bc0dbce940c38577e0a82fb0cdcdaa0ffa4
-- <smoke571reg@yandex.ru>
2026-06-16 14:23:56 +0000

    Updated eslint config paths checked & added comments

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,35 @@
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
+const FILES = ["dashboard_frontend/**/*.{ts,tsx,js,jsx}"];
+
 
 export default tseslint.config(
     {
+        // NOTE: FILES passed to recommended configs does not currently
+        // prevent eslint from walking over all project dirs,
+        // so large ones are explicitly excluded
         ignores: [
             "node_modules/**",
+            ".venv/**",
             "dashboard_frontend/dist/**",
-            ".venv/**"
         ]
     },
 
-    js.configs.recommended,
-    ...tseslint.configs.recommended,
     {
-        // files: [
-        //     "dashboard_frontend/**/*.{ts,tsx,js,jsx}"
-        // ],
-        // rules: {
-        //     '@typescript-eslint/no-explicit-any': 'warn',
-        // },
-    }
+        ...js.configs.recommended,
+        files: FILES,
+    },
+    ...tseslint.configs.recommended.map(config => ({
+        ...config,
+        files: FILES,
+    })),
+
+    // // Custom rules can be added here
+    // {
+    //     files: FILES,
+    //     rules: {
+    //         '@typescript-eslint/no-explicit-any': 'warn',
+    //     },
+    // }
 );
