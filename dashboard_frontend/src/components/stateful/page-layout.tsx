191595db36b944dfcb8ead7db007b0b7644bef6d
-- <smoke571reg@yandex.ru>
2026-06-22 15:56:07 +0000

    Increased main content max fullscreen width

--- dashboard_frontend/src/components/stateful/page-layout.tsx
+++ dashboard_frontend/src/components/stateful/page-layout.tsx
@@ -1,4 +1,5 @@
 import { Navbar } from "@/components/stateful/navbar";
+import { cn } from "@/styles/utils";
 
 /**
  * App shell: renders the top navbar and a centered content area.
@@ -8,7 +9,18 @@ export const PageLayout = ({ children }: { children: React.ReactNode }) => {
     return (
         <div className="min-h-screen bg-background">
             <Navbar />
-            <main className="mx-auto max-w-6xl px-4 py-8 [&>:not(:last-child)]:mb-4">
+            <main
+                className={cn(
+                    // Center horizontally
+                    "mx-auto",
+                    // 100 % stacked, 80 % fullscreen, capped at 1536 (1920 * 0.8)px
+                    "w-full lg:w-4/5 lg:max-w-[1536px]",
+                    // Inner padding
+                    "px-4 py-8",
+                    // Separate direct children
+                    "[&>:not(:last-child)]:mb-4",
+                )}
+            >
                 {children}
             </main>
         </div>
