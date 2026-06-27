d7ddcbfae4461316dc335a90491d6593bbe4530c
-- <smoke571reg@yandex.ru>
2026-06-27 08:27:57 +0000

    Moved <ChartsContainer>

--- dashboard_frontend/src/components/page-parts/visualizations/mdx/russia-state-budget/chart-group/category-chart-group.tsx
+++ dashboard_frontend/src/components/page-parts/visualizations/mdx/russia-state-budget/chart-group/category-chart-group.tsx
@@ -16,7 +16,7 @@ import { CategoryLineChart } from "./charts/category-line-chart";
 import { CategoryShareStackedBarChart } from "./charts/category-share-stacked-bar-chart";
 import { CategoryTreemap } from "./charts/category-treemap";
 import { CategoryDiffTable } from "./charts/category-diff-table";
-import { ChartsContainer } from "../../../charts-container";
+import { ChartsContainer } from "@/components/common/visualizations/charts/charts-container";
 
 import type { RussiaStateBudgetItem } from "@/types/visualization-data/russia-state-budget";
 import type { CategoryInfo } from "./selectors/category-hierarchy";
