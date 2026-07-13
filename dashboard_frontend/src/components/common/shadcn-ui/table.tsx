ed233270284c320105c54b974b95726bd7e30ba6
-- <smoke571reg@yandex.ru>
2026-07-13 16:40:39 +0000

    Updated table header & hover styles

--- dashboard_frontend/src/components/common/shadcn-ui/table.tsx
+++ dashboard_frontend/src/components/common/shadcn-ui/table.tsx
@@ -56,7 +56,7 @@ const TableRow = React.forwardRef<
     <tr
         ref={ref}
         className={cn(
-            "border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted",
+            "border-b transition-colors hover:bg-accent hover:text-accent-foreground data-[state=selected]:bg-muted",
             className,
         )}
         {...props}
@@ -71,7 +71,7 @@ const TableHead = React.forwardRef<
     <th
         ref={ref}
         className={cn(
-            "h-12 px-4 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0",
+            "h-12 px-4 text-left align-middle font-bold text-foreground [&:has([role=checkbox])]:pr-0",
             className,
         )}
         {...props}
